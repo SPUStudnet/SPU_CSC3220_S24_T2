76be31014749e9e5be48a8f88bfb6a517f1f5587
SPUStudnet <155692979+SPUStudnet@users.noreply.github.com>
2024-05-08 21:33:42 -0700

    Added database schema and DDL doc.

---
+++ Design/Documents/Database/SQLQueries/SelectAll.sql
@@ -0,0 +1,8 @@
+SELECT * FROM Dish;
+SELECT * FROM Dish_Ingredient_Lookup;
+SELECT * FROM Dish_Tags_Lookup;
+SELECT * FROM Ingredients; 
+SELECT * FROM Meal_Dish_Lookup;
+SELECT * FROM Mealtime, Tags;
+SELECT * FROM Meal;
+
